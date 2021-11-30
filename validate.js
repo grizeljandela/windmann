c47831cc2a0127b4d1d2314abcd47772c9ec6e90
Cedric Mingham <2021650@stud.hs-mannheim.de>
2021-11-30 21:12:33 +0100

    validate.js Prüfung verbessert

--- validate.js
+++ validate.js
@@ -1,67 +1,74 @@
-/* Clientseitige Eingabepruefungen */
+/* Clientseitige Eingabepruefungen versandadresse.html */
 
 console.log("validate.js geladen");
 
+const form = document.getElementById("formular");
+
+form.addEventListener("submit", (e) => {
+
+  if (!pruefeEingabe()) {
+    e.preventDefault();
+  }
+
+});
+
 function pruefeEingabe() {
 
-  console.log("pruefeEingabe()");
+  console.log("call pruefeEingabe()");
 
-  const form = document.getElementById("formular")
 
   if (form != null) {
 
     var regex;
     var onError = false;
+    var errorListDebug = [];
 
-    /* Iteriere durch alle input-Elemente*/
     Array.from(form.elements).forEach((input) => {
 
       switch (input.type) {
 
         case "text":
-        /* leer ("") wird schon im html gerprüft*/
-        regex = new RegExp("^[a-zA-Z- ]+$");
-        /* von Anfang an a-z A-Z, wobei diese mind 1. mal vorkommen müssen */
+
+        regex = new RegExp("^[a-zA-Z- ]{4,}$");
 
         if (input.id === "Adresszusatz") {
           regex = new RegExp("^[a-zA-Z0-9- ]+$");
 
         }else if (input.id === "BIC") {
-          /* Wikipedia ISO_9362 */
+          /* ISO 9362 */
           regex = new RegExp("^(([A-Z0-9]{4}[A-Z]{2}[A-Z0-9]{2})|([A-Z0-9]{4}[A-Z]{2}[A-Z0-9]{2}[A-Z0-9]{3}))$");
 
         }else if (input.id === "IBAN") {
-          /* ISO  ISO 13616 */
+          /* ISO 13616 */
           regex = new RegExp("^[A-Z]{2}[0-9]{2}[a-zA-Z0-9]{16,30}$");
         }
+
         break;
 
         case "number":
+
         regex = new RegExp("^[0-9]*$");
-        /*Am Anfang dezimal 0-beliebiger Länge, dazwischen 0-9 und am Ende wieder 0-beliebig*/
 
         if(input.id === "PLZ") {
           regex = new RegExp("^([0]{1}[1-9]{1}|[1-9]{1}[0-9]{1})[0-9]{3}$");
-          /* (von Anfang an 01 oder 10) und 0-9 3 mal*/
+    
         }else if (input.id === "Telefonnumer") {
           regex = new RegExp("^[0-9]{8,}$");
         }
+
         break;
 
-        /*Wir nehmen nur die HTML5-Prüfung
-        case "email":
-        regex = new RegExp("^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$");
-        break;*/
-
         default:
+
         regex = null;
-        console.log(input.id+": unbehandeltes Eingabefeld");
+
         break;
       }
 
       try{
+
         if((regex != null) && (regex.test(input.value) == false)) {
-          throw{ stderr: "enthält ungültige Zeichen"};
+          throw{ stderr: input.id};
         }else{
           input.style.border = null;
         }
@@ -69,13 +76,13 @@ function pruefeEingabe() {
       }catch(e) {
         onError = true;
         input.setAttribute("style", "border:2px solid #ff0000;");
-        console.log(input.id+": "+e.stderr);
+        errorListDebug.push(e.stderr);
       }
 
     });
 
     if (onError == false) {
-      console.log("onError: "+onError);
+      console.log("alles OK");
       return true;
     }
 
@@ -83,7 +90,8 @@ function pruefeEingabe() {
   }else{
     console.log("FEHLER: form ist null");
   }
-  console.log("onError: "+onError);
+  console.log("onError = true");
+  console.log("Fehler in Formularfeldern: "+errorListDebug);
 
   appendError();
 
@@ -94,12 +102,12 @@ function appendError() {
 
   let error_out = document.createElement("p");
 
-  error_out.appendChild(document.createTextNode("Bitte beheben Sie die Fehler im Formular."));
+  error_out.appendChild(document.createTextNode("Bitte beheben Sie die markierten Fehler im Formular."));
 
   error_out.setAttribute("style", "color: red; padding-top: 20px;");
 
   error_out.setAttribute("id", "stderr");
-  error_out.setAttribute("class", "versand_input");
+  error_out.setAttribute("class", "all_inclusiv");
 
   if (document.getElementById("stderr") == null) {
 
