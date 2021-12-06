a0a7babe46f44a2b8155a55dedd47c2f5f7d38fa
a.grizelj <a.grizelj@stud.hs-mannheim.de>
2021-12-06 12:06:07 +0100

    Produktsortierung Blaskraft und Homepage Empfehlung überarbeitet

--- sort_products.js
+++ sort_products.js
@@ -2,22 +2,64 @@ const a = [
     {
         name: "WM 2500",
         preis: "250",
-        bild: "images/wm_2500.png",
+        bild: "images/wm_2500.JPG",
         blaskraft: "8"
     },
     {
         name: "WM 50 Lite",
         preis: "100",
-        bild: "images/wm_50_lite.png",
+        bild: "images/wm_50_lite.jpg",
         blaskraft: "5"
     },
     {
         name: "WM super 300",
         preis: "500",
-        bild: "images/wm_super_300.png",
+        bild: "images/wm_super_300.JPG",
         blaskraft: "20"
+    },
+    {
+        name: "WM 1000",
+        preis: "150",
+        bild: "images/wm_1000.jpg",
+        blaskraft: "21"
+    },
+    {
+        name: "WM 2600",
+        preis: "320",
+        bild: "images/wm_2600.JPG",
+        blaskraft: "9"
+    },
+    {
+        name: "WM Super 22",
+        preis: "600",
+        bild: "images/wm_super_22.JPG",
+        blaskraft: "15"
+    },
+    {
+        name: "WM 55 Lite",
+        preis: "50",
+        bild: "images/wm_55_lite.JPG",
+        blaskraft: "25"
+    },
+    {
+        name: "WM Super 121",
+        preis: "800",
+        bild: "images/wm_super_121.JPG",
+        blaskraft: "10"
+    },
+    {
+        name: "WM 1111",
+        preis: "1150",
+        bild: "images/wm_1111.JPG",
+        blaskraft: "22"
+    },
+    {
+        name: "WM 8000",
+        preis: "1000",
+        bild: "images/wm_8000.JPG",
+        blaskraft: "30"
     }
-]
+];
 
 const frame = document.querySelector(".produkte-frame");
 
@@ -37,14 +79,18 @@ function generateProductsOntoPage() {
         let price = document.createElement("p");
         price.classList.add("ps-preis");
         let bild = document.createElement("img");
+        let bk = document.createElement("p");
+        bk.classList.add("blaskraft_id");
 
         name.innerHTML = a[i].name;
         price.innerHTML = "Preis: " + a[i].preis + "€";
+        bk.innerHTML = a[i].blaskraft + "N";
         bild.setAttribute("src",a[i].bild);
 
         box.appendChild(bild);
         box.appendChild(name);
         box.appendChild(price);
+        box.appendChild(bk);
 
         link.appendChild(box);
 
