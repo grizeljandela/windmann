be7d6a96afb6e9d86d77e469c3b939615c288dc3
c.isik <1912156@stud.hs-mannheim.de>
2022-01-12 11:23:34 +0100

    formular-check

--- versandadresse.php
+++ versandadresse.php
@@ -32,7 +32,7 @@
 					<li><a href = "index.html">Home</a></li>
 					<li><a href = "produkte.php">Produkte</a></li>
 					<li><a href = "service.html">Service</a></li>
-					<li><a href = "community.php">Community</a></li>
+					<li><a href = "community.html">Community</a></li>
 					<li><a href = "impressum.html">Impressum</a></li>
 					<li id="nav_highlighted"><a href = "warenkorb.html">Warenkorb</a></li>
 				</ul>
@@ -40,17 +40,85 @@
 			</div>
 		</header>
 
+	<?php
+			$versandDaten = array ();
+
+			$name_bool=$vorname_bool=$straße_bool=$hausNr_bool
+			=$straße_bool=$plz_bool=$wohnort_bool=$email_bool=$telNr_bool=$kontoinhaber_bool
+			=$bic_bool=$iban_bool=$agbs_bool=true;
+
+			if(!isset($_POST['Name'])||(strlen($_POST['Name'])< 2)) {
+				$name_bool=false;
+
+			}elseif(!isset($_POST['Vorname'])||(strlen($_POST['Vorname'])< 2)){
+				$vorname_bool=false;
+
+			}elseif(!isset($_POST['Straße'])||(strlen($_POST['Straße'])< 3)){
+				$strasse_bool=false;
+
+			}elseif(!isset($_POST['Hausnummer'])||(strlen($_POST['Hausnummer'])< 1)){
+				$hausNr_bool=false;
+
+			}elseif(!isset($_POST['Straße'])||(strlen($_POST['Straße'])< 1)){
+				$straße_bool=false;
+
+			}elseif(!isset($_POST['PLZ'])||strlen($_POST['PLZ'])< 4 || intval($_POST['PLZ']) <= 0){
+				$plz_bool=false;
+
+			}elseif(!isset($_POST['Wohnort'])||strlen($_POST['Wohnort'])< 4){
+				$wohnort_bool=false;
+
+			}elseif(!filter_var($_POST['E-MAil-Adresse'], FILTER_VALIDATE_EMAIL)){
+				$email_bool=false;
+
+			}elseif(!preg_match("/^([0-9]{10})$/", $_POST['Telefonnummer'])){
+				$telNr_bool=false;
+
+			}
+
+			$regexp = '/^[A-Z]{6,6}[A-Z2-9][A-NP-Z0-9]([A-Z0-9]{3,3}){0,1}$/i';
+
+
+			if(!isset($_POST['Kontoinhaber']) || empty($_POST['Kontoinhaber']) ||strlen($_POST['Name'])< 2){
+				$kontoinhaber_bool=false;
+			}elseif(!preg_match($regexp, $_POST['BIC'])){
+				$bic_bool=false;
+			}elseif(!preg_match('/^[A-Z]{2}[0-9]{2}[A-Z0-9]{1,30}$/', $_POST['IBAN'])|| empty($_POST['IBAN'])){
+				$iban_bool=false;
+			}elseif(!isset($_POST['AGBs'])){
+				$agbs_bool=false;
+
+			}
+
+			if(($name_bool && $vorname_bool&&$straße_bool&&$hausNr_bool
+				&&$straße_bool&&$plz_bool&&$wohnort_bool&&$email_bool&&$telNr_bool&&$kontoinhaber_bool
+				&&$bic_bool&&$iban_bool&&$agbs_bool)){
+				$versandDaten['Name'] = $_POST['Name'];
+				$versandDaten['Vorname'] = $_POST['Vorname'];
+				$versandDaten['Straße'] = $_POST['Straße'];
+				$versandDaten['Hausnummer'] = $_POST['Hausnummer'];
+				$versandDaten['Plz'] = $_POST['PLZ'];
+				$versandDaten['Wohnort'] = $_POST['Wohnort'];
+				$versandDaten['E-Mail-Adresse'] = $_POST['E-MAil-Adresse'];
+				$versandDaten['TelefonNr'] = $_POST['Telefonnummer'];
+				$versandDaten['Kontoinhaber'] = $_POST['Kontoinhaber'];
+				$versandDaten['BIC'] = $_POST['BIC'];
+				$versandDaten['IBAN'] = $_POST['IBAN'];
+				$versandDaten['agbs'] = $_POST['AGBs'];
+			}
+
+	?>
+ 
 	<!-- Content -->
-		<main class="content">
+	<main class="content">
 			<div class="titel_versandadresse">
 				<img src="images/versandadresse.jpg" alt="Versandadresse"/>
 				<h1>Versandadresse</h1>
 			</div>
-
-			<form id="formular" action="bestellbestaetigung.html" method="post">
+			<form id="formular" action="versandadresse.php" method="post">
 		<div class="all_inclusiv">
-				<label class="label_versand" for="Name"> Name</label>
-				<input class ="versand_input" type="text" id="Name" name="Name" autofocus required />
+			<label class="label_versand" for="Name"> Name</label>
+				<input class ="versand_input" type="text" id="Name" name="Name" autofocus required/>
 				<br />
 				<label class="label_versand" for="Vorname"> Vorname</label>
 				<input class ="versand_input" type="text" id="Vorname" name="Vorname" required />
@@ -59,13 +127,13 @@
 				<input class ="versand_input" type="text" id="Strasse" name="Straße" required />
 				<br />
 				<label class="label_versand" for="Hausnummer"> Hausnummer</label>
-				<input class ="versand_input" type="number" id="Hausnummer" name="Hausnummer" min="0" required />
+				<input class ="versand_input" type="number" id="Hausnummer" name="Hausnummer" required />
 				<br />
 				<label class="label_versand" for="Adresszusatz"> Adresszusatz</label>
 				<input class ="versand_input" type="text" id="Adresszusatz" name="Adresszusatz" />
 				<br />
 				<label class="label_versand" for="PLZ"> PLZ</label>
-				<input class ="versand_input" type="number" id="Plz" name="PLZ" required />
+				<input class ="versand_input" type="number" id="Plz" name="PLZ" min="3" required />
 				<br />
 				<label class="label_versand" for="Wohnort"> Wohnort</label>
 				<input class ="versand_input" type="text" id="Wohnort" name="Wohnort" required />
@@ -74,25 +142,28 @@
 				<input class ="versand_input" type="email" id="Mail" name="E-MAil-Adresse" required />
 				<br />
 				<label class="label_versand" for="Telefon"> Tel.Nummer</label>
-				<input class ="versand_input" type="number" id="Telefon" name="Telefonnummer" required />
+				<input class ="versand_input" type="number" id="Telefon" name="Telefonnummer" min="4" max ="20" required />
 
 			<h2>Zahlungsart</h2>
 			<h3>Lastschriftmandat</h3>
 
 				<div class="konto">
 						<label class="label_versand" for="Kontoinhaber"> Kontoinhaber</label>
-						<input class ="versand_input" type="text" id="Kontoinhaber" name="Kontoinhaber" required />
+						<input class ="versand_input" type="text" id="Kontoinhaber" name="Kontoinhaber"  >
 						<br />
 						<label class="label_versand" for="BIC"> BIC</label>
-						<input class ="versand_input" type="text" id="Bic" name="BIC" required />
+						<input class ="versand_input" type="text" id="Bic" name="BIC"  />
 						<br />
 						<label class="label_versand" for="IBAN"> IBAN</label>
-						<input class ="versand_input" type="text" id="Iban" name="IBAN" required />
+						<input class ="versand_input" type="text" id="Iban" name="IBAN" />
 						<br/>
-						<input type="checkbox" id="AGBs" name="AGBs" required />
+						<input type="checkbox" id="AGBs" name="AGBs"  />
 						<label for="AGBs"> Ich akzeptiere die genannten Datenschutzbedingungen</label>
-
+						<br/>
 				</div>
+
+
+
 			<div class="buttons_versand">
 				<input class ="versand_input" type="submit" value="Bestätigen" style="max-width:200px"/>
 				<input class ="versand_input" type="reset" value="Angaben löschen" style="max-width:200px"/>
